1fcc13df1d1e16eefe654606a52d72ab02304201
Josh Waller <jrwall11@utexas.edu>
2015-07-14 00:27:01 -0500

    Corrected overwrite of conf.json & added gamescrap.py

---
+++ backend/gamescrap.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+data = urlopen('https://en.wikipedia.org/wiki/List_of_NCAA_conferences')
+soup = BeautifulSoup(data.read(), "html.parser")
