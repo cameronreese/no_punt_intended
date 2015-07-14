296ea23c6b31342ee432808313b51bb440ab9532
Josh Waller <jrwall11@utexas.edu>
2015-07-14 00:25:47 -0500

    Added to models.py and fillDB.py

---
+++ db/fillDB.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# -----------
+# imports
+# -----------
+from models import players, teams, conf, games, schedule
+from flask import Flask
+from models import punt, db
+import json
+import sys
+
