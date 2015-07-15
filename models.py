14f4bce057549c2587aca5c32c2143999a8fc9ee
Cameron Reese <cameronreese@mac.com>
2015-07-15 11:37:06 -0500

    unit test attempts

--- models.py
+++ models.py
@@ -91,8 +91,8 @@ def get_teams(team_name):
     if team_name == 'teams':
         return teams.query.all()
     else:
-        qryresult = teams.query.get(team_name)
-        return jsonify(json_list=[i.serialize for i in qryresult.all()])
+        #qryresult = teams.query.get(team_name)
+        return jsonify(json_list=[i.serialize for i in teams.query.get(team_name)])
 
 @punt.route('/punt/conf/<string:conf_name>', methods=['GET'])
 def get_conf(conf_name):
